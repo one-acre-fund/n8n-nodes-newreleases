b2c804d3a02b41169454b1a8ddc1475041b27698
Yann Jouanique <yann.jouanique@gmail.com>
2022-11-23 12:23:45 +0200

    Support for project creation

--- nodes/NewReleases/NewReleases.node.ts
+++ nodes/NewReleases/NewReleases.node.ts
@@ -1,5 +1,6 @@
 import { IExecuteFunctions } from 'n8n-core';
 import {
+	IDataObject,
 	INodeExecutionData,
 	INodeType,
 	INodeTypeDescription,
@@ -94,6 +95,21 @@ export class NewReleases implements INodeType {
 				default: 'list',
 				options: [
 					{
+						name: 'Create Projects',
+						value: 'create',
+						action: 'Create a new project',
+					},
+					{
+						name: 'Delete Project',
+						value: 'delete',
+						action: 'Delete project a project',
+					},
+					{
+						name: 'Get Project',
+						value: 'get',
+						action: 'Get project a project',
+					},
+					{
 						name: 'List Projects',
 						value: 'list',
 						action: 'List projects a project',
@@ -102,16 +118,6 @@ export class NewReleases implements INodeType {
 						name: 'Search Projects',
 						value: 'search',
 						action: 'Search projects a project',
-					},
-					{
-						name: 'Get Project',
-						value: 'get',
-						action: 'Get project a project',
-					},
-					{
-						name: 'Delete Project',
-						value: 'delete',
-						action: 'Delete project a project',
 					},
 				],
 			},
@@ -234,6 +240,284 @@ export class NewReleases implements INodeType {
 					},
 				},
 			},
+
+			// provider
+			{
+				displayName: 'Provider (e.g. Github, Docker Hub...)',
+				name: 'provider',
+				type: 'string',
+				default: 'github',
+				description: 'See https://newreleases.io for a list of supported options',
+				displayOptions: {
+					show: {
+						resource: ['project'],
+						operation: ['create'],
+					},
+				},
+			},
+
+			// name
+			{
+				displayName: 'Project Name',
+				name: 'name',
+				type: 'string',
+				default: '',
+				description: 'User-friendly name',
+				displayOptions: {
+					show: {
+						resource: ['project'],
+						operation: ['create'],
+					},
+				},
+			},
+
+			// email_notification
+			{
+				displayName: 'Email_notification',
+				name: 'email_notification',
+				type: 'options',
+				default: 'none',
+				description: 'Email Notification Frequency',
+				displayOptions: {
+					show: {
+						resource: ['project'],
+						operation: ['create'],
+					},
+				},
+				options: [
+					{
+						name: 'Account Default',
+						value: 'default',
+					},
+					{
+						name: 'Daily',
+						value: 'daily',
+					},
+					{
+						name: 'Hourly',
+						value: 'hourly',
+					},
+					{
+						name: 'None',
+						value: 'none',
+					},
+					{
+						name: 'Weekly',
+						value: 'weekly',
+					},
+				],
+			},
+
+			// exclude_prereleases
+			{
+				displayName: 'Exclude Pre-Releases?',
+				name: 'exclude_prereleases',
+				type: 'boolean',
+				default: false,
+				displayOptions: {
+					show: {
+						resource: ['project'],
+						operation: ['create'],
+					},
+				},
+			},
+
+			// exclude_updated
+			{
+				displayName: 'Exclude Updated?',
+				name: 'exclude_updated',
+				type: 'boolean',
+				default: false,
+				displayOptions: {
+					show: {
+						resource: ['project'],
+						operation: ['create'],
+					},
+				},
+			},
+
+			// note
+			{
+				displayName: 'Note',
+				name: 'note',
+				type: 'string',
+				typeOptions: {
+					rows: 4,
+				},
+				default: '',
+				displayOptions: {
+					show: {
+						resource: ['project'],
+						operation: ['create'],
+					},
+				},
+			},
+
+			{
+				displayName: 'Exclude Version Regex',
+				name: 'exclude_version_regexp',
+				type: 'fixedCollection',
+				placeholder: 'Add New Exclusion Regex',
+				typeOptions: {
+					multipleValues: true,
+				},
+				default: [],
+				options: [
+					{
+						name: 'exclusion',
+						displayName: 'Exclusion Regex',
+						values: [
+							{
+								displayName: 'Regex',
+								name: 'value',
+								type: 'string',
+								default: '',
+							},
+							{
+								displayName: 'Include?',
+								name: 'inverse',
+								type: 'boolean',
+								default: false,
+								description: 'Whether to use the regex as an inclusion mask',
+							},
+						],
+					},
+				],
+			},
+
+			// TODO: These require using IDs rather than free text, so we'd need to look them up...
+			// {
+			// 	displayName: 'Advanced Options',
+			// 	name: 'createOptions',
+			// 	type: 'collection',
+			// 	default: {},
+			// 	displayOptions: {
+			// 		show: {
+			// 			resource: ['project'],
+			// 			operation: ['create'],
+			// 		},
+			// 	},
+			// 	options: [
+			// 		// slack_channels
+			// 		{
+			// 			displayName: 'Slack Channels',
+			// 			name: 'slack_channels',
+			// 			type: 'string',
+			// 			typeOptions: {
+			// 				multipleValues: true,
+			// 				multipleValueButtonText: 'Add Notification Channel',
+			// 			},
+			// 			default: '',
+			// 		},
+
+			// 		// telegram_chats
+			// 		{
+			// 			displayName: 'Telegram Chats',
+			// 			name: 'telegram_chats',
+			// 			type: 'string',
+			// 			typeOptions: {
+			// 				multipleValues: true,
+			// 				multipleValueButtonText: 'Add Notification Channel',
+			// 			},
+			// 			default: '',
+			// 		},
+
+			// 		// discord_channels
+			// 		{
+			// 			displayName: 'Discord Channels',
+			// 			name: 'discord_channels',
+			// 			type: 'string',
+			// 			typeOptions: {
+			// 				multipleValues: true,
+			// 				multipleValueButtonText: 'Add Notification Channel',
+			// 			},
+			// 			default: '',
+			// 		},
+
+			// 		// hangouts_chat_webhooks
+			// 		{
+			// 			displayName: 'Hangouts Chat Webhooks',
+			// 			name: 'hangouts_chat_webhooks',
+			// 			type: 'string',
+			// 			typeOptions: {
+			// 				multipleValues: true,
+			// 				multipleValueButtonText: 'Add Notification Channel',
+			// 			},
+			// 			default: '',
+			// 		},
+
+			// 		// microsoft_teams_webhooks
+			// 		{
+			// 			displayName: 'Microsoft Teams Webhooks',
+			// 			name: 'microsoft_teams_webhooks',
+			// 			type: 'string',
+			// 			typeOptions: {
+			// 				multipleValues: true,
+			// 				multipleValueButtonText: 'Add Notification Channel',
+			// 			},
+			// 			default: '',
+			// 		},
+
+			// 		// mattermost_webhooks
+			// 		{
+			// 			displayName: 'Mattermost Webhooks',
+			// 			name: 'mattermost_webhooks',
+			// 			type: 'string',
+			// 			typeOptions: {
+			// 				multipleValues: true,
+			// 				multipleValueButtonText: 'Add Notification Channel',
+			// 			},
+			// 			default: '',
+			// 		},
+
+			// 		// rocketchat_webhooks
+			// 		{
+			// 			displayName: 'Rocketchat Webhooks',
+			// 			name: 'rocketchat_webhooks',
+			// 			type: 'string',
+			// 			typeOptions: {
+			// 				multipleValues: true,
+			// 				multipleValueButtonText: 'Add Notification Channel',
+			// 			},
+			// 			default: '',
+			// 		},
+
+			// 		// matrix_rooms
+			// 		{
+			// 			displayName: 'Matrix Rooms',
+			// 			name: 'matrix_rooms',
+			// 			type: 'string',
+			// 			typeOptions: {
+			// 				multipleValues: true,
+			// 				multipleValueButtonText: 'Add Notification Channel',
+			// 			},
+			// 			default: '',
+			// 		},
+
+			// 		// webhooks
+			// 		{
+			// 			displayName: 'Webhooks',
+			// 			name: 'webhooks',
+			// 			type: 'string',
+			// 			typeOptions: {
+			// 				multipleValues: true,
+			// 				multipleValueButtonText: 'Add Notification Channel',
+			// 			},
+			// 			default: '',
+			// 		},
+
+			// 		{
+			// 			displayName: 'Tags',
+			// 			name: 'tags',
+			// 			type: 'string',
+			// 			typeOptions: {
+			// 				multipleValues: true,
+			// 				multipleValueButtonText: 'Add Tag',
+			// 			},
+			// 			default: '',
+			// 		},
+			// 	],
+			// },
 		],
 	};
 
@@ -319,6 +603,56 @@ export class NewReleases implements INodeType {
 					responseData = await newReleasesApiRequest.call(this, {
 						method: 'DELETE',
 						url: `/v1/projects/${slug}`,
+					});
+
+					returnData = returnData.concat(responseData);
+				}
+			}
+
+			if (operation === 'create') {
+				// ----------------------------------
+				//          Project::create
+				// ----------------------------------
+				for (let i = 0; i < items.length; i++) {
+					const provider = this.getNodeParameter('provider', i, null) as string;
+					const name = this.getNodeParameter('name', i, null) as string;
+					const email_notification = this.getNodeParameter('email_notification', i, null) as string;
+					const exclude_prereleases = this.getNodeParameter(
+						'exclude_prereleases',
+						i,
+						false,
+					) as boolean;
+					const exclude_updated = this.getNodeParameter('exclude_updated', i, false) as boolean;
+					const note = this.getNodeParameter('note', i, null) as string;
+
+					// const createOptions = this.getNodeParameter('createOptions', i, null) as IDataObject;
+					const exclude_version_regexp = this.getNodeParameter(
+						'exclude_version_regexp',
+						i,
+						null,
+					) as IDataObject;
+
+					const body = {
+						name,
+						provider,
+						email_notification,
+						exclude_prereleases,
+						exclude_updated,
+						note,
+
+						...(exclude_version_regexp &&
+							(exclude_version_regexp as any).exclusion && {
+								exclude_version_regexp: (exclude_version_regexp as any).exclusion,
+							}),
+					};
+
+					console.dir(exclude_version_regexp);
+					console.dir(body);
+
+					responseData = await newReleasesApiRequest.call(this, {
+						method: 'POST',
+						url: `/v1/projects`,
+						body,
 					});
 
 					returnData = returnData.concat(responseData);
